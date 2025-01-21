e6836ac2a0ef9c5e3d6f1f45bc5dd2fb271d55af
Учеба <you@example.com>
2025-01-21 18:04:33 +0300

    Add function for save russian names to file

--- src/main.py
+++ src/main.py
@@ -28,11 +28,17 @@ def filter_russian_names(names_list: list) -> list:
             new_names_list.append(name_item)
     return new_names_list
 
+
+def save_to_file(file_name: str, data: str) -> None:
+    """ Сохраняет данные в файл"""
+    with open('../data/' + file_name,'w') as names_file:
+        names_file.write(data)
+
+
 if __name__ == "__main__":
     cleared_names = clear_names("names.txt")
 
-    print(filter_russian_names(cleared_names))
-
-
-    # for i in cleared_names:
-    #     print(i)
+    save_to_file(
+        'russian_names.txt',
+        '\n'.join(filter_russian_names(cleared_names))
+    )
